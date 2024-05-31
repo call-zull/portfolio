0781fde0ee274f8c43002f7ae2c83f39f3cc3426
ijuldi <ijuldi77@gmail.com>
2024-05-31 14:52:06 +0800

    edit 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  HashRouter as Router, // Menggunakan HashRouter di sini
+  BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
